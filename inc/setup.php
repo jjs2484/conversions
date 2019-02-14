40f397279cf9d8042037d44e2e628aab29fc8211
uniquelylost <jsneider24@gmail.com>
2019-02-14 13:43:56 -0500

    remove languages folder

--- inc/setup.php
+++ inc/setup.php
@@ -25,13 +25,6 @@ if ( ! function_exists ( 'conversions_setup' ) ) {
 	 * as indicating support for post thumbnails.
 	 */
 	function conversions_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on conversions, use a find and replace
-		 * to change 'conversions' to the name of your theme in all the template files
-		 */
-		load_theme_textdomain( 'conversions', get_template_directory() . '/languages' );
 
 		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
