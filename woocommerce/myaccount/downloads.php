71f210fcd02c7f1c7328949d76a4cbdfe0dc4121
uniquelylost <jsneider24@gmail.com>
2019-04-04 18:29:35 -0400

    Add better buttons in WC account

--- woocommerce/myaccount/downloads.php
+++ woocommerce/myaccount/downloads.php
@@ -37,7 +37,7 @@ do_action( 'woocommerce_before_account_downloads', $has_downloads ); ?>
 
 <?php else : ?>
 	<div class="woocommerce-Message woocommerce-Message--info woocommerce-info">
-		<a class="btn btn-outline-primary" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
+		<a class="woocommerce-Button button" href="<?php echo esc_url( apply_filters( 'woocommerce_return_to_shop_redirect', wc_get_page_permalink( 'shop' ) ) ); ?>">
 			<?php esc_html_e( 'Go shop', 'conversions' ) ?>
 		</a>
 		<?php esc_html_e( 'No downloads available yet.', 'conversions' ); ?>
