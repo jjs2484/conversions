0aa9562f926e68d321797ee279a892f6151fc41a
uniquelylost <jsneider24@gmail.com>
2019-08-01 13:08:04 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -9,19 +9,19 @@
 defined( 'ABSPATH' ) || exit;
 
 $conversions_includes = array(
-	'/setup.php',                           // Theme setup and custom theme supports.
-	'/widgets.php',                         // Register widget area.
-	'/enqueue.php',                         // Enqueue scripts and styles.
-	'/template-tags.php',                   // Custom template tags for this theme.
-	'/pagination.php',                      // Custom pagination for this theme.
-	'/extras.php',                          // Custom functions.
-	'/customizer.php',              		// Customizer additions.
-	'/customizer-social.php',               // Customizer social icons.
-	'/customizer-nav.php',               	// Customizer nav filter with options.
-	'/customizer-output.php',               // Customizer output styles.
-	'/custom-comments.php',                 // Custom Comments file.
-	'/class-wp-bootstrap-navwalker.php',    // Load custom WordPress nav walker.
-	'/woocommerce.php',                     // Load WooCommerce functions.
+	'/setup.php',							// Theme setup and custom theme supports.
+	'/widgets.php',							// Register widget area.
+	'/enqueue.php',							// Enqueue scripts and styles.
+	'/template-tags.php',					// Custom template tags for this theme.
+	'/pagination.php',						// Custom pagination for this theme.
+	'/extras.php',							// Custom functions.
+	'/customizer.php',						// Customizer additions.
+	'/customizer-social.php',				// Customizer social icons.
+	'/customizer-nav.php',					// Customizer nav filter with options.
+	'/customizer-output.php',				// Customizer output styles.
+	'/custom-comments.php',					// Custom Comments file.
+	'/class-wp-bootstrap-navwalker.php',	// Load custom WordPress nav walker.
+	'/woocommerce.php',						// Load WooCommerce functions.
 );
 
 foreach ( $conversions_includes as $file ) {
