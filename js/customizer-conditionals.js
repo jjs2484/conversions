c8e166ac6d581d9fc6f78c926cc20214ecdd170a
uniquelylost <jsneider24@gmail.com>
2019-11-06 11:57:16 -0500

    add conditional to show nav button options

--- js/customizer-conditionals.js
+++ js/customizer-conditionals.js
@@ -1,9 +1,35 @@
 /**
  * Customizer conditionals
  */
+
+/* Navbar button options */
 jQuery(document).ready(function ($) {
 
-    /* Clients section responsive breakpoint items */
+    /* button option selectors */
+    var navbar_btn_options = $( '#customize-control-conversions_nav_button_text_control, #customize-control-conversions_nav_button_url_control' );
+
+    /* on page load hide or show options */
+    if( $( '#customize-control-conversions_nav_button select' ).val() == 'no' ){
+        navbar_btn_options.hide();
+    }
+    else {
+        navbar_btn_options.show();
+    }
+
+    /* on change hide or show options */
+    $( '#customize-control-conversions_nav_button select' ).change(function(){
+        if($(this).val() == 'no') {
+            navbar_btn_options.hide();
+        } else {
+            navbar_btn_options.show();
+        }
+    });
+});
+
+/* Clients section responsive breakpoints */
+jQuery(document).ready(function ($) {
+
+    /* responsive breakpoint items selectors */
     var manual_sizes = $( '#customize-control-conversions_hc_sm_control, #customize-control-conversions_hc_md_control, #customize-control-conversions_hc_lg_control' );
 
     /* on page load hide or show options */
