db932d4e76b4d196d6552794cc3d76b595cb9b97
uniquelylost <jsneider24@gmail.com>
2019-11-30 00:56:02 -0500

    more unique pricing table feature IDs

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -326,7 +326,8 @@ get_header();
                           $cpricing_feature_count = 0;
                           foreach( $feature_repeater as $value ) {
                             // Output each feature
-                            echo sprintf( '<li id="c-pricing__feature-%1$s">%2$s</li>', 
+                            echo sprintf( '<li id="c-pricing__feature-%1$s-%2$s">%3$s</li>', 
+                              esc_attr( $cpricing_table_count ),
                               esc_attr( $cpricing_feature_count ),
                               esc_html( $value->feature )
                             );
