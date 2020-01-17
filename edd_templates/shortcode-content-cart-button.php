811d58c568e6e3713fafb3879fc1b8fdd2d4b8e0
uniquelylost <jsneider24@gmail.com>
2020-01-16 21:09:58 -0500

    some edd store styles

--- edd_templates/shortcode-content-cart-button.php
+++ edd_templates/shortcode-content-cart-button.php
@@ -1,3 +1,3 @@
 <div class="edd_download_buy_button">
-	<?php echo edd_get_purchase_link( array( 'class' => 'btn btn-lg btn-block btn-primary', 'price' => false, 'download_id' => get_the_ID() ) ); ?>
+	<?php echo edd_get_purchase_link( array( 'class' => 'btn btn-lg btn-block btn-outline-primary', 'price' => false, 'download_id' => get_the_ID() ) ); ?>
 </div>
