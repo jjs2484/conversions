668f4c3c0e74e93136b92c4f4a8f7f80736c3c22
uniquelylost <jsneider24@gmail.com>
2020-01-19 11:48:34 -0500

    EDD style refinements

--- edd_templates/shortcode-content-price.php
+++ edd_templates/shortcode-content-price.php
@@ -1,15 +1,21 @@
 <?php
-	// Get the download ID
-	$download_id = get_the_ID();
+	echo '<div>
+		<div class="edd_price">';
 
-	// Get the prices
-	if ( edd_is_free_download( $download_id ) ) {
-		$price = '<div class="edd_price">' . __( 'Free', 'conversions' ) . '</div>';
- 	} elseif ( edd_has_variable_prices( $download_id ) ) {
-		$price = '<div class="edd_price">' . edd_price_range( $download_id ) . '</div>';
-	} else {
-		$price = '<div class="edd_price">' . edd_price( $download_id ) . '</div>';
-	}
+			// Get the download ID
+			$download_id = get_the_ID();
 
-	echo $price;
+			// Get the prices
+			if ( edd_is_free_download( $download_id ) ) {
+				$price = __( 'Free', 'conversions' );
+ 			} elseif ( edd_has_variable_prices( $download_id ) ) {
+				$price = edd_price_range( $download_id );
+			} else {
+				$price = edd_price( $download_id );
+			}
+
+			echo $price;
+
+		echo '</div>
+	</div>';
 ?>
