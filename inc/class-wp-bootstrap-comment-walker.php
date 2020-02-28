479b5c9cb82b3113c039336038a201038deac212
uniquelylost <jsneider24@gmail.com>
2020-02-27 22:11:58 -0500

    refactor comment walker and styles

--- inc/class-wp-bootstrap-comment-walker.php
+++ inc/class-wp-bootstrap-comment-walker.php
@@ -1,15 +1,8 @@
 <?php
 /**
- * Plugin Name: WP Bootstrap Comments
- * Plugin URI:  http://darrinb.com/plugins/wp-bootstrap-comments
- * Description: Easily build valid Bootstrap v3 comment lists. Includes nested comments.
- * Version:     0.1.0
- * Author:      Darrin Boutote
- * Author URI:  http://darrinb.com
- * License:     GPL2
- * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * WP Bootstrap Comment Walker
  *
- * NOTE: Credited comment walker has been modified to work with Bootstrap 4.
+ * @since 02/27/20
  *
  * @package conversions
  */
@@ -22,145 +15,29 @@ namespace conversions;
  * @extends Walker_Comment
  */
 class WP_Bootstrap_Comment_Walker extends \Walker_Comment {
+
 	/**
-	 * Start the element output.
+	 * Outputs a comment in the HTML5 format.
 	 *
-	 * This opens the comment.  Will check if the comment has children or is a stand-alone comment.
-	 *
-	 * @access public
-	 * @since 0.1.0
-	 *
-	 * @see Walker::start_el()
 	 * @see wp_list_comments()
+	 * @see https://developer.wordpress.org/reference/functions/get_comment_author_url/
+	 * @see https://developer.wordpress.org/reference/functions/get_comment_author/
+	 * @see https://developer.wordpress.org/reference/functions/get_avatar/
+	 * @see https://developer.wordpress.org/reference/functions/get_comment_reply_link/
+	 * @see https://developer.wordpress.org/reference/functions/get_edit_comment_link/
 	 *
-	 * @global int        $comment_depth
-	 * @global WP_Comment $comment
-	 *
-	 * @param string $output  Passed by reference. Used to append additional content.
-	 * @param object $comment Comment data object.
-	 * @param int    $depth   Depth of comment in reference to parents.
-	 * @param array  $args    An array of arguments.
-	 * @param int    $id      ID.
-	 */
-	public function start_el( &$output, $comment, $depth = 0, $args = array(), $id = 0 ) {
-		$depth++;
-		$GLOBALS['comment_depth'] = $depth;
-		$GLOBALS['comment']       = $comment;
-		if ( ! empty( $args['callback'] ) ) {
-			ob_start();
-			call_user_func( $args['callback'], $comment, $args, $depth );
-			$output .= ob_get_clean();
-			return;
-		}
-		if ( ( 'pingback' == $comment->comment_type || 'trackback' == $comment->comment_type ) && $args['short_ping'] ) {
-			ob_start();
-			$this->ping( $comment, $depth, $args );
-			$output .= ob_get_clean();
-		} elseif ( 'html5' === $args['format'] ) {
-			ob_start();
-			if ( ! empty( $args['has_children'] ) ) {
-				$this->start_parent_html5_comment( $comment, $depth, $args );
-			} else {
-				$this->html5_comment( $comment, $depth, $args );
-			}
-			$output .= ob_get_clean();
-		} else {
-			ob_start();
-			$this->comment( $comment, $depth, $args );
-			$output .= ob_get_clean();
-		}
-	}
-	/**
-	 * Ends the element output, if needed.
-	 *
-	 * This ends the comment.  Will check if the comment has children or is a stand-alone comment.
-	 *
-	 * @access public
-	 * @since 0.1.0
-	 *
-	 * @see Walker::end_el()
-	 * @see wp_list_comments()
-	 *
-	 * @param string     $output  Passed by reference. Used to append additional content.
-	 * @param WP_Comment $comment The comment object. Default current comment.
-	 * @param int        $depth   Depth of comment.
+	 * @param WP_Comment $comment Comment to display.
+	 * @param int        $depth   Depth of the current comment.
 	 * @param array      $args    An array of arguments.
 	 */
-	public function end_el( &$output, $comment, $depth = 0, $args = array() ) {
-		if ( ! empty( $args['end-callback'] ) ) {
-			ob_start();
-			call_user_func( $args['end-callback'], $comment, $args, $depth );
-			$output .= ob_get_clean();
-			return;
-		}
-		if ( ! empty( $args['has_children'] ) && 'html5' === $args['format'] ) {
-			ob_start();
-			$this->end_parent_html5_comment( $comment, $depth, $args );
-			$output .= ob_get_clean();
-		} else {
-			if ( 'div' == $args['style'] ) {
-				$output .= "</div><!-- #comment-## -->\n";
-			} else {
-				$output .= "</li><!-- #comment-## -->\n";
-			}
-		}
-	}
-	/**
-	 * Output the beginning of a parent comment in the HTML5 format.
-	 *
-	 * Bootstrap media element requires child comments to be nested within the parent media-body.
-	 * The original comment walker writes the entire comment at once, this method writes the opening
-	 * of a parent comment so children comments can be nested within.
-	 *
-	 * @access protected
-	 * @since 0.1.0
-	 *
-	 * @see http://getbootstrap.com/components/#media
-	 * @see wp_list_comments()
-	 *
-	 * @param object $comment Comment to display.
-	 * @param int    $depth   Depth of comment.
-	 * @param array  $args    An array of arguments.
-	 */
-	protected function start_parent_html5_comment( $comment, $depth, $args ) {
-		$this->html5_comment( $comment, $depth, $args, $is_parent = true );
-	}
-	/**
-	 * Output a comment in the HTML5 format.
-	 *
-	 * @access protected
-	 * @since 0.1.0
-	 *
-	 * @see wp_list_comments()
-	 *
-	 * @param object  $comment   Comment to display.
-	 * @param int     $depth     Depth of comment.
-	 * @param array   $args      An array of arguments.
-	 * @param boolean $is_parent Flag indicating whether or not this is a parent comment.
-	 */
-	protected function html5_comment( $comment, $depth, $args, $is_parent = false ) {
-		$tag             = ( 'div' === $args['style'] ) ? 'div' : 'li';
-		$type            = get_comment_type();
-		$comment_classes = array();
+	protected function html5_comment( $comment, $depth, $args ) {
 
-		// if it's a parent.
-		if ( $this->has_children ) {
-			$comment_classes[] = 'parent';
-			$comment_classes[] = 'has-children';
-		}
-		// if it's a child.
-		if ( $comment->comment_parent > 0 ) {
-			$comment_classes[] = 'child';
-			$comment_classes[] = 'has-parent';
-			$comment_classes[] = 'parent-' . $comment->comment_parent;
-		}
-		$comment_classes = apply_filters( 'wp_bootstrap_comment_class', $comment_classes, $comment, $depth, $args );
-		$class_str       = implode( ' ', $comment_classes );
+		$tag  = ( 'div' === $args['style'] ) ? 'div' : 'li';
+		$type = get_comment_type();
+
 		?>
-		<<?php echo esc_html( $tag ); ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( $class_str, $comment ); ?>>
-
-			<div class="media">
-
+		<<?php echo $tag; //phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- static output ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( $this->has_children ? 'parent' : '', $comment ); ?>>
+			<article id="div-comment-<?php comment_ID(); ?>" class="comment-body media">
 				<div class="commenter d-flex flex-row mb-1">
 
 					<?php if ( 0 != $args['avatar_size'] && 'pingback' !== $type && 'trackback' !== $type ) { ?>
@@ -230,69 +107,8 @@ class WP_Bootstrap_Comment_Walker extends \Walker_Comment {
 							</li>
 						</ul><!-- comment actions -->
 					</div>
-
-					<?php if ( $is_parent ) { ?>
-						<div class="child-comments">
-					<?php } else { ?>
-						</div><!-- /.media-body -->
-						</div><!-- /.media -->
-					<?php } ?>
-
-		<?php
-	}
-	/**
-	 * Output the end of a parent comment in the HTML5 format.
-	 *
-	 * Bootstrap media element requires child comments to be nested within the parent media-body.
-	 * The original comment walker writes the entire comment at once, this method writes the end
-	 * of a parent comment so child comments can be nested within.
-	 *
-	 * @see http://getbootstrap.com/components/#media
-	 *
-	 * @access protected
-	 * @since 0.1.0
-	 *
-	 * @see wp_list_comments()
-	 *
-	 * @param object $comment Comment to display.
-	 * @param int    $depth   Depth of comment.
-	 * @param array  $args    An array of arguments.
-	 */
-	protected function end_parent_html5_comment( $comment, $depth, $args ) {
-		$tag = ( 'div' === $args['style'] ) ? 'div' : 'li';
-		?>
-					</div><!-- /.child-comments -->
 				</div><!-- /.media-body -->
-			</div><!-- /.media -->
-		</<?php echo esc_html( $tag ); ?>><!-- /.parent -->
-
-		<?php
-	}
-	/**
-	 * Output a pingback comment.
-	 *
-	 * @access protected
-	 * @since 0.1.0
-	 *
-	 * @see wp_list_comments()
-	 *
-	 * @param WP_Comment $comment The comment object.
-	 * @param int        $depth   Depth of comment.
-	 * @param array      $args    An array of arguments.
-	 */
-	protected function ping( $comment, $depth, $args ) {
-		$tag               = ( 'div' == $args['style'] ) ? 'div' : 'li';
-		$comment_classes   = array();
-		$comment_classes[] = 'media';
-		$comment_classes   = apply_filters( 'wp_bootstrap_comment_class', $comment_classes, $comment, $depth, $args );
-		$class_str         = implode( ' ', $comment_classes );
-		?>
-		<<?php echo esc_html( $tag ); ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( $class_str, $comment ); ?>>
-			<div class="comment-body">
-				<div class="media-body">
-					<?php esc_html_e( 'Pingback:', 'conversions' ); ?> <?php comment_author_link( $comment ); ?> <?php edit_comment_link( __( 'Edit', 'conversions' ), '<span class="edit-link">', '</span>' ); ?>
-				</div><!-- /.media-body -->
-			</div><!-- /.comment-body -->
+			</article><!-- .media -->
 		<?php
 	}
 
