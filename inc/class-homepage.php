4d7a011c51ce0675d6b4f815cf0426d290cb63dd
uniquelylost <jsneider24@gmail.com>
2020-03-10 22:05:45 -0400

    Update class-homepage.php

--- inc/class-homepage.php
+++ inc/class-homepage.php
@@ -330,7 +330,7 @@ class Homepage {
 											</button>
 											<!-- 16:9 aspect ratio -->
 											<div class="embed-responsive embed-responsive-16by9">
-												<iframe class="embed-responsive-item" src="" id="video" allow="autoplay"></iframe>
+												<iframe class="embed-responsive-item" src="" id="video" allow="autoplay" allowfullscreen></iframe>
 											</div>
 										</div>
 									</div>
