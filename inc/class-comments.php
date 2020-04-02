c6cb83b2637e6ad2d4eaeb268a570f8c39995ae6
uniquelylost <jsneider24@gmail.com>
2020-04-01 21:51:22 -0400

    better escaping for comment fields

--- inc/class-comments.php
+++ inc/class-comments.php
@@ -46,10 +46,10 @@ class Comments {
 			'email'   => '<div class="form-group comment-form-email"><label for="email">'
 				. __( 'Email', 'conversions' ) . ( $req ? ' <span class="required">*</span>' : '' )
 				. '</label> ' . '<input class="form-control" id="email" name="email" '
-				. ( $html5 ? 'type="email"' : 'type="text"' ) . ' value="' . esc_attr( $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . '></div>',
+				. ( $html5 ? 'type="email"' : 'type="text"' ) . ' value="' . sanitize_email( $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . '></div>',
 			'url'     => '<div class="form-group comment-form-url"><label for="url">'
 				. __( 'Website', 'conversions' ) . '</label> ' . '<input class="form-control" id="url" name="url" '
-				. ( $html5 ? 'type="url"' : 'type="text"' ) . ' value="' . esc_attr( $commenter['comment_author_url'] ) . '" size="30"></div>',
+				. ( $html5 ? 'type="url"' : 'type="text"' ) . ' value="' . esc_url( $commenter['comment_author_url'] ) . '" size="30"></div>',
 			'cookies' => '<div class="form-group form-check comment-form-cookies-consent"><input class="form-check-input" id="wp-comment-cookies-consent" name="wp-comment-cookies-consent" type="checkbox" value="yes"'
 				. $consent . ' /> ' . '<label class="form-check-label" for="wp-comment-cookies-consent">'
 				. __( 'Save my name, email, and website in this browser for the next time I comment', 'conversions' ) . '</label></div>',
