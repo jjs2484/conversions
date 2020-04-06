f60b1a55bf12c0338fc56361e4016cee8bc877cd
uniquelylost <jsneider24@gmail.com>
2020-04-06 01:53:32 -0400

    remove HP customizer conditionals

--- js/customizer-conditionals.js
+++ js/customizer-conditionals.js
@@ -56,12 +56,8 @@ jQuery(document).ready(function($) {
 jQuery(document).ready(function($) {
 
 	var conditionalOptions = [
-		[ '#customize-control-conversions_hh_button_text_control, #customize-control-conversions_hh_button_url_control', '#customize-control-conversions_hh_button', 'no'],
-		[ '#customize-control-conversions_hh_vbtn_text_control, #customize-control-conversions_hh_vbtn_url_control', '#customize-control-conversions_hh_vbtn', 'no'],
 		[ '#customize-control-conversions_hcta_btn_text_control, #customize-control-conversions_cta_btn_url_control', '#customize-control-conversions_hcta_btn', 'no'],
 		[ '#customize-control-conversions_nav_button_text_control, #customize-control-conversions_nav_button_url_control', '#customize-control-conversions_nav_button', 'no'],
-		[ '#customize-control-conversions_hc_sm_control, #customize-control-conversions_hc_md_control, #customize-control-conversions_hc_lg_control', '#customize-control-conversions_hc_respond', 'auto'],
-		[ '#customize-control-conversions_pricing_sm_control, #customize-control-conversions_pricing_md_control, #customize-control-conversions_pricing_lg_control', '#customize-control-conversions_pricing_respond', 'auto'],
 	];
 	
 	conditionalOptions.forEach( function( conditionalOptionsArray ) {
@@ -85,30 +81,5 @@ jQuery(document).ready(function($) {
 				$( $conditionalSelectors ).show();
 			}
 		});
-	});
-});
-
-/*
- * EDD product options
- */
-jQuery(document).ready(function($) {
-
-	// Option selectors.
-	var $eddProductTypeTax = $( '#customize-control-conversions_edd_product_tax_control' );
-	var $eddProductTypeChoice = $( '#customize-control-conversions_edd_products select' );
-
-	// On page load hide or show options.
-	if( $( $eddProductTypeChoice ).val() == 'no' || $( $eddProductTypeChoice ).val() == 'all' ) {
-		$( $eddProductTypeTax ).hide();
-	}
-
-	// On change hide or show options.
-	$( $eddProductTypeChoice ).change(function() {
-		if ( $(this).val() == 'category' || $(this).val() == 'tags' ) {
-			$( $eddProductTypeTax ).show();
-		} 
-		else if ( $(this).val() == 'no' || $(this).val() == 'all' ) {
-			$( $eddProductTypeTax ).hide();
-		}
 	});
 });
