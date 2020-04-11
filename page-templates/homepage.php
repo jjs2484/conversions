66644f422f9127f279a4e022901c16aa41f1f96e
uniquelylost <jsneider24@gmail.com>
2020-04-11 12:06:46 -0400

    add notice on homepage if extensions are not detected

--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -20,9 +20,33 @@ get_header();
 
 <div id="homepage-wrapper" class="wrapper content-wrapper">
 
-<?php
-do_action( 'homepage' ); // phpcs:ignore WPThemeReview.CoreFunctionality.PrefixAllGlobals.NonPrefixedHooknameFound
-?>
+	<?php
+	// If conversions extensions plugin isn't installed lets recommend it.
+	if ( ! class_exists( '\conversions\Homepage' ) ) {
+		if ( current_user_can( 'edit_theme_options' ) ) { // Display this only to those with the right access.
+			?>
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-12">
+						<div class="alert alert-primary mt-5 mb-5" role="alert">
+							<?php
+							printf(
+								/* translators: %s: plugin link */
+								esc_html__( 'You need %s activated to display the homepage sections. Activate the plugin first!', 'conversions' ),
+								'<a href="' . esc_url( 'https://wordpress.org/plugins/conversions-extensions/', 'conversions' ) . '" class="alert-link">' . esc_html__( 'Conversions Extensions', 'conversions' ) . '</a>'
+							);
+							?>
+						</div>
+					</div>
+				</div>
+			</div>
+			<?php
+		}
+	}
+
+	// Output homepage.
+	do_action( 'homepage' ); // phpcs:ignore WPThemeReview.CoreFunctionality.PrefixAllGlobals.NonPrefixedHooknameFound
+	?>
 
 </div><!-- Wrapper end -->
 
