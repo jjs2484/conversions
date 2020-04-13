d1ec1771734db1fdab6f54de60145ade8a59ed64
uniquelylost <jsneider24@gmail.com>
2020-04-13 13:19:04 -0400

    Mobile menu focus trapping script

--- js/theme.js
+++ js/theme.js
@@ -170,3 +170,70 @@ jQuery(document).ready(function() {
 		jQuery( '#video' ).attr( 'src', $videoSrc ); 
 	});
 });
+
+/**
+ * Mobile menu focus trapping 
+ */
+jQuery( document ).ready( function( $ ) {
+	var $menu_elements = $( 'button.navbar-toggler, nav.navbar a.nav-link, nav.navbar .nav-callout-button a' );
+ 
+	// Rewrite the elements to listen to tab and shift tab, and focus on each other.
+	$menu_elements.keydown( function( e ) {
+		if ( $( this ).attr( 'focus_locked' ) != 'true' )
+			return;
+		var code = e.keyCode || e.which;
+		if ( code != 9 )
+			return;
+
+		var $current_element = $( this );
+
+		// Let the focus go to the submenu item if this is a submenu parent.
+		if ( $current_element.attr( 'aria-expanded' ) == 'true' )
+			return;
+ 
+		e.preventDefault();
+		var counter;
+		for( counter = 0; counter < $menu_elements.length ; counter++ ) {
+			if( $current_element[0 ] == $menu_elements[ counter ] )
+				break;
+		}
+ 
+		// Tab forwards
+		if ( ! e.shiftKey ) {
+			if ( counter == $menu_elements.length - 1 )
+				// This is the last element. Select first.
+				$menu_elements.first().focus();
+			else
+				$menu_elements[ counter + 1 ].focus();
+		}
+		else
+		{
+			// Tab backwards!
+			if ( counter > 0 )
+				$menu_elements[ counter - 1 ].focus();
+			else
+			{
+				// First element. Select last.
+				$menu_elements.last().focus();
+			}
+		}
+ 
+	} );
+ 
+	var $navbar_toggler = $( 'button.navbar-toggler' );
+	// Lock the focus to the elements within.
+	$navbar_toggler.click( function( e ) {
+		if ( $navbar_toggler.attr( 'aria-expanded' ) != 'true' ) {
+			$menu_elements.attr( 'focus_locked', true );
+			// We need a tiny, tiny timeout to allow for the element to focus.
+			setTimeout( function() {
+				$menu_elements.first().focus();
+			}, 10 );
+		}
+		else
+		{
+			$menu_elements.removeAttr( 'focus_locked' );
+			$( this ).blur();
+		}
+	} );
+} );
