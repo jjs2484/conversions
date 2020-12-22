d1560a30772e44b1507cb6286ca57abe6396527b
Jesse S <3428918+jjs2484@users.noreply.github.com>
2020-12-22 01:04:17 -0500

    update TGMPA settings

--- inc/Conversions.php
+++ inc/Conversions.php
@@ -241,25 +241,30 @@ namespace conversions
 		public function conversions_register_required_plugins() {
 
 			// Include plugin from the WordPress Plugin Repository.
-			$plugins = array(
-				array(
+			$plugins = [
+				[
 					'name'     => 'Conversions Extensions',
 					'slug'     => 'conversions-extensions',
 					'required' => false,
-				),
-			);
+				],
+				[
+					'name'     => 'One Click Demo Import',
+					'slug'     => 'one-click-demo-import',
+					'required' => false,
+				],
+			];
 
 			// Array of configuration settings. Amend each line as needed.
-			$config = array(
+			$config = [
 				'id'           => 'conversions',           // Unique ID for hashing notices for multiple instances of TGMPA.
 				'default_path' => '',                      // Default absolute path to bundled plugins.
 				'menu'         => 'tgmpa-install-plugins', // Menu slug.
 				'has_notices'  => true,                    // Show admin notices or not.
 				'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
 				'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-				'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+				'is_automatic' => true,                    // Automatically activate plugins after installation or not.
 				'message'      => '',                      // Message to output right before the plugins table.
-			);
+			];
 
 			tgmpa( $plugins, $config );
 		}
