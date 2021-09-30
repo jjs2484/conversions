1f71b52425f93e2799828044d88e44c3601310c0
Jesse S <3428918+jjs2484@users.noreply.github.com>
2021-09-30 08:42:49 -0400

    enable BS popovers and tooltips everywhere

--- js/theme.js
+++ js/theme.js
@@ -1,4 +1,24 @@
 /**
+ * Enable Bootstrap popovers everywhere 
+*/
+jQuery(document).ready(function() {
+	var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+	var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+		return new bootstrap.Popover(popoverTriggerEl);
+	});
+});
+
+/**
+ * Enable Bootstrap tooltips everywhere 
+*/
+jQuery(document).ready(function() {
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+		return new bootstrap.Tooltip(tooltipTriggerEl);
+	});
+});
+
+/**
  * If fixed header calc height and add margin to content 
 */
 jQuery(function() {
