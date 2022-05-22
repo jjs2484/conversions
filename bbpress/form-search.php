f1667a217950a98c8f6548ce606b5a3400f5fe2f
Jesse S <3428918+jjs2484@users.noreply.github.com>
2022-05-22 08:46:23 -0400

    better bbPress BS5 search form

---
+++ bbpress/form-search.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Search
+ *
+ * @package bbPress
+ * @subpackage Theme
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+if ( bbp_allow_search() ) :
+	?>
+
+	<div class="bbp-search-form">
+		<form role="search" method="get" id="bbp-search-form">
+			<div class="input-group">
+				<label class="screen-reader-text hidden" for="bbp_search"><?php esc_html_e( 'Search for:', 'bbpress' ); ?></label>
+				<input type="hidden" name="action" value="bbp-search-request" />
+				<input type="text" class="form-control" value="<?php bbp_search_terms(); ?>" name="bbp_search" id="bbp_search" />
+				<button class="btn btn-outline-secondary" type="submit" id="bbp_search_submit"><?php esc_attr_e( 'Search', 'bbpress' ); ?></button>
+			</div>
+		</form>
+	</div>
+
+	<?php
+endif;
