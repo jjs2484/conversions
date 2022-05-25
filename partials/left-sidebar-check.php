5a4b4c6a8a663043b0e9569dc4d0e71b52e08425
Jesse S <3428918+jjs2484@users.noreply.github.com>
2022-05-25 13:54:23 -0400

    better side padding when conversions_content_cards active

--- partials/left-sidebar-check.php
+++ partials/left-sidebar-check.php
@@ -7,41 +7,38 @@
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
-?>
 
-<?php
 $conversions_sidebar_pos = get_theme_mod( 'conversions_sidebar_position', 'right' );
-?>
+if ( get_theme_mod( 'conversions_content_cards', false ) === true ) {
+	$conversions_sidebar_padding = '4';
+} else {
+	$conversions_sidebar_padding = '5';
+}
 
-<?php if ( 'left' === $conversions_sidebar_pos ) : ?>
-	<?php get_template_part( 'partials/sidebar', 'left' ); ?>
-<?php endif; ?>
+if ( 'left' === $conversions_sidebar_pos ) :
+	get_template_part( 'partials/sidebar', 'left' );
+endif;
 
-<?php
 // primary content area columns based on selected and active sidebars.
 // if left sidebar is selected.
 if ( 'left' === $conversions_sidebar_pos ) {
 	if ( is_active_sidebar( 'sidebar-2' ) ) {
-		?>
-		<div class="col-lg-9 ps-lg-5 content-area" id="primary">
-		<?php
+		echo sprintf(
+			'<div class="col-lg-9 ps-lg-%s content-area" id="primary">',
+			esc_attr( $conversions_sidebar_padding )
+		);
 	} else {
-		?>
-		<div class="col-md-12 content-area" id="primary">
-		<?php
+		echo '<div class="col-md-12 content-area" id="primary">';
 	}
 } elseif ( 'right' === $conversions_sidebar_pos ) { // if right sidebar is selected.
 	if ( is_active_sidebar( 'sidebar-1' ) ) {
-		?>
-		<div class="col-lg-9 pe-lg-5 content-area" id="primary">
-		<?php
+		echo sprintf(
+			'<div class="col-lg-9 pe-lg-%s content-area" id="primary">',
+			esc_attr( $conversions_sidebar_padding )
+		);
 	} else {
-		?>
-		<div class="col-md-12 content-area" id="primary">
-		<?php
+		echo '<div class="col-md-12 content-area" id="primary">';
 	}
 } elseif ( 'none' === $conversions_sidebar_pos ) { // if no sidebar is selected.
-	?>
-	<div class="col-md-12 content-area" id="primary">
-	<?php
+	echo '<div class="col-md-12 content-area" id="primary">';
 }
