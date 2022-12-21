9ec978b371b2938884a537f5a0d0e834f9d57698
Jesse S <3428918+jjs2484@users.noreply.github.com>
2022-12-20 22:09:43 -0500

    Update quantity-input.php

--- woocommerce/global/quantity-input.php
+++ woocommerce/global/quantity-input.php
@@ -11,41 +11,60 @@
  * the readme will list any important changes.
  *
  * @see     https://docs.woocommerce.com/document/template-structure/
- * @package WooCommerce/Templates
- * @version 4.0.0
+ * @package WooCommerce\Templates
+ * @version 7.2.1
  * @phpcs:disable WPThemeReview.CoreFunctionality.PrefixAllGlobals.NonPrefixedHooknameFound
  * @phpcs:disable WPThemeReview.CoreFunctionality.PrefixAllGlobals.NonPrefixedVariableFound
  */
 
 defined( 'ABSPATH' ) || exit;
 
+/* translators: %s: Quantity. */
+$label = ! empty( $args['product_name'] ) ? sprintf( esc_html__( '%s quantity', 'conversions' ), wp_strip_all_tags( $args['product_name'] ) ) : esc_html__( 'Quantity', 'conversions' );
+
+// In some cases we wish to display the quantity but not allow for it to be changed.
 if ( $max_value && $min_value === $max_value ) {
-	?>
-	<div class="quantity hidden">
-		<input type="hidden" id="<?php echo esc_attr( $input_id ); ?>" class="qty" name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $min_value ); ?>" />
-	</div>
-	<?php
+	$is_readonly = true;
+	$input_value = $min_value;
 } else {
-	/* translators: %s: Quantity. */
-	$label = ! empty( $args['product_name'] ) ? sprintf( esc_html__( '%s quantity', 'conversions' ), wp_strip_all_tags( $args['product_name'] ) ) : esc_html__( 'Quantity', 'conversions' );
-	?>
-	<div class="quantity">
-		<?php do_action( 'woocommerce_before_quantity_input_field' ); ?>
-		<label class="visually-hidden" for="<?php echo esc_attr( $input_id ); ?>"><?php echo esc_html( $label ); ?></label>
-		<input
-			type="number"
-			id="<?php echo esc_attr( $input_id ); ?>"
-			class="<?php echo esc_attr( join( ' ', (array) $classes ) ); ?>"
-			step="<?php echo esc_attr( $step ); ?>"
-			min="<?php echo esc_attr( $min_value ); ?>"
-			max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>"
-			name="<?php echo esc_attr( $input_name ); ?>"
-			value="<?php echo esc_attr( $input_value ); ?>"
-			title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'conversions' ); ?>"
-			size="4"
-			placeholder="<?php echo esc_attr( $placeholder ); ?>"
-			inputmode="<?php echo esc_attr( $inputmode ); ?>" />
-		<?php do_action( 'woocommerce_after_quantity_input_field' ); ?>
-	</div>
-	<?php
+	$is_readonly = false;
 }
+?>
+<div class="quantity">
+	<?php
+	/**
+	 * Hook to output something before the quantity input field.
+	 *
+	 * @since 7.2.0
+	 */
+	do_action( 'woocommerce_before_quantity_input_field' );
+	?>
+	<label class="visually-hidden" for="<?php echo esc_attr( $input_id ); ?>"><?php echo esc_attr( $label ); ?></label>
+	<input
+		type="<?php echo $is_readonly ? 'text' : 'number'; ?>"
+		<?php echo $is_readonly ? 'readonly="readonly"' : ''; ?>
+		id="<?php echo esc_attr( $input_id ); ?>"
+		class="<?php echo esc_attr( join( ' ', (array) $classes ) ); ?>"
+		name="<?php echo esc_attr( $input_name ); ?>"
+		value="<?php echo esc_attr( $input_value ); ?>"
+		title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'conversions' ); ?>"
+		size="4"
+		min="<?php echo esc_attr( $min_value ); ?>"
+		max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>"
+		<?php if ( ! $is_readonly ): ?>
+			step="<?php echo esc_attr( $step ); ?>"
+			placeholder="<?php echo esc_attr( $placeholder ); ?>"
+			inputmode="<?php echo esc_attr( $inputmode ); ?>"
+			autocomplete="<?php echo esc_attr( isset( $autocomplete ) ? $autocomplete : 'on' ); ?>"
+		<?php endif; ?>
+	/>
+	<?php
+	/**
+	 * Hook to output something after quantity input field
+	 *
+	 * @since 3.6.0
+	 */
+	do_action( 'woocommerce_after_quantity_input_field' );
+	?>
+</div>
+<?php
